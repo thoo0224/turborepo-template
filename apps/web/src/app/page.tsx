f7ea3c52304767abcd99aa6c118de64391d3e0e8
Thomas <40025225+thoo0224@users.noreply.github.com>
2023-08-04 20:03:42 +0200

    prisma

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,8 +1,11 @@
-import { Button } from "@cfxstore/ui/index";
+import { Button } from "@cfxstore/ui";
 import * as logger from "@cfxstore/logger";
+import { PrismaClient } from "@cfxstore/database";
 
-export default function Page() {
-  logger.log("info", "test", "Index");
+export default async function Page() {
+  const prisma = new PrismaClient();
+  const users = await prisma.user.findMany();
+  console.log(users);
 
   return (
     <div className="m-3 bg-red-500 p-3 text-fuchsia-50">
