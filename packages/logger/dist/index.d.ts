02e9779b0265bb0ec97be35a70f0f3392fb599ab
Thomas <40025225+thoo0224@users.noreply.github.com>
2023-08-04 20:08:57 +0200

    okg

--- packages/logger/dist/index.d.ts
+++
@@ -1,4 +0,0 @@
-type LogLevel = "info" | "warn" | "debug" | "error" | "verbose";
-export declare function log(level: LogLevel, message: string, context?: string): void;
-export {};
-//# sourceMappingURL=index.d.ts.map
